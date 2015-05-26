a93da96a7dfdd289faae5016f62b738a53bed6a0
Dan <Hyperparticle@gmail.com>
2015-05-25 18:29:21 -0600

    Interfaced GUI Buttons

--- Calculime/Calculime/MainWindow.xaml.cs
+++ Calculime/Calculime/MainWindow.xaml.cs
@@ -46,18 +46,65 @@ namespace Calculime
 		{
 			string expression = InputTextBox.Text;
 
-			HistoryListView.Items.Add(new MyItem { Id = expression });
 			OutputTextBlock.Text = parser.Parse(expression);
+			HistoryListView.Items.Add(new HistoryItem { Id = expression + '=' + OutputTextBlock.Text});
 		}
 
-		public class MyItem
+		private void ClearButton_Click(object sender, RoutedEventArgs e)
 		{
-			public string Id { get; set; }
+			InputTextBox.Text = "";
 		}
 
-		private void HistoryListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		private void DeleteButton_Click(object sender, RoutedEventArgs e)
 		{
+			string text = InputTextBox.Text;
 
+			InputTextBox.Text = text.Substring(0, text.Length - 1);
+		}
+
+		private void NumberButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (e.Source == ZeroButton)
+				InputTextBox.Text += 0;
+			else if (e.Source == OneButton)
+				InputTextBox.Text += 1;
+			else if (e.Source == TwoButton)
+				InputTextBox.Text += 2;
+			else if (e.Source == ThreeButton)
+				InputTextBox.Text += 3;
+			else if (e.Source == FourButton)
+				InputTextBox.Text += 4;
+			else if (e.Source == FiveButton)
+				InputTextBox.Text += 5;
+			else if (e.Source == SixButton)
+				InputTextBox.Text += 6;
+			else if (e.Source == SevenButton)
+				InputTextBox.Text += 7;
+			else if (e.Source == EightButton)
+				InputTextBox.Text += 8;
+			else if (e.Source == NineButton)
+				InputTextBox.Text += 9;
+			else if (e.Source == DecimalButton)
+				InputTextBox.Text += '.';
+		}
+
+		private void OperationButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (e.Source == MultiplyButton)
+				InputTextBox.Text += '*';
+			else if (e.Source == DivideButton)
+				InputTextBox.Text += '/';
+			else if (e.Source == AddButton)
+				InputTextBox.Text += '+';
+			else if (e.Source == SubtractButton)
+				InputTextBox.Text += '-';
+			else if (e.Source == PowerButton)
+				InputTextBox.Text += '^';
 		}
     }
+
+	public class HistoryItem
+	{
+		public string Id { get; set; }
+	}
 }
